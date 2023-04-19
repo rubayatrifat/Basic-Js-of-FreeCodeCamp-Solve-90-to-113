0cb2e9d636c3062539d7137520a28bce0d95e4e0
rubayatrifat <rubayatislam17@gmail.com>
2023-04-19 16:06:00 +0600

    FCC Problem Solve

---
+++ 93 - Solve of FCC/main.js
@@ -0,0 +1,41 @@
+// Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.
+
+// myMusic should be an array
+// myMusic should have at least two elements
+// The elements in the myMusic array should be objects
+// Your object in myMusic should have at least 4 properties
+// Your object in myMusic should contain the property artist which is a string
+// Your object in myMusic should contain the property title which is a string
+// Your object in myMusic should contain the property release_year which is a number
+// Your object in myMusic should contain a formats property which is an array
+
+
+
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Michael Jackson",
+      "title": "Thriller",
+      "release_year": 1982,
+      "formats": [
+        "CD",
+        "8T",
+        "LP",
+        "Cassette"
+      ],
+      "gold": true
+    }
+  ];
+  
+  console.log(myMusic);
+  
